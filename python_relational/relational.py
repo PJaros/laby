996c124f99762e713f8f83221b8c7de42d53fa6a
Paul Jaros <jaros.paul@gmail.com>
2022-06-14 22:56:15 +0200

    dig_labyrinth hinzugefügt

--- python_relational/relational.py
+++ python_relational/relational.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-sizeX, sizeY = (2, 6)
+from random import choice, randint
+
+sizeX, sizeY = (100, 20)
 unused_pos = []
-wall_str = "#"*(sizeX*2+1)
-# dirs = ((0,-1), (1, 0), (0, 1), (-1, 0))
+dirs = ((0,-1), (1, 0), (0, 1), (-1, 0))
 for y in range(sizeY):
     for x in range(sizeX):
         unused_pos.append((x, y))
@@ -25,33 +26,52 @@ class tunnel:
         self.points[pos1] = p1
         self.points[pos2] = p2
 
-def show_laby(laby):
-    print(wall_str)
-    for y in range(sizeY):
-        for x in range(sizeX+1):
-            laby_point = laby.points.get((x, y) or None)
-            if laby_point and (x-1, y) in laby_point.connect:
-                print(" ", end='')
-            else:
-                print("#", end='')
-            if x <= sizeX-1:
-                if laby_point:
-                    print(" ", end='')
-                else:
-                    print("#", end='')
-        print()
-        for x in range(sizeX):
-            laby_point = laby.points.get((x, y) or None)
-            if laby_point and (x, y+1) in laby_point.connect:
-                print("# ", end='')
-            else:
-                print("##", end='')
-        print("#")
 
-laby = tunnel()
-laby.add((0, 0), (1, 0))
-laby.add((1, 0), (1, 1))
-laby.add((1, 1), (1, 2))
+def coord(n):
+    return n * 2 + 1
 
 
-show_laby(laby)
+def show(laby):
+    li = [coord(sizeX) * ['#'] for _ in range(coord(sizeY))]
+    for p in laby.points.values():
+        x, y = coord(p.pos[0]), coord(p.pos[1])
+        li[y][x] = ' '
+        for p_connect in p.connect:
+            xc, yc = coord(p_connect[0]), coord(p_connect[1])
+            x_mean, y_mean = (x + xc) // 2, (y + yc) // 2
+            li[y_mean][x_mean] = ' '
+
+    print("\n".join(["".join(elem) for elem in li]))
+
+
+def dig_labyrinth():
+    laby = tunnel()
+    dig = (0, 0)
+    unused_pos.remove(dig)
+    jumpPos = []
+
+    while len(unused_pos) > 0:
+        while True:
+            avaiPos = []
+            for d in dirs:
+                npos = dig[0] + d[0], dig[1] + d[1]
+                if npos in unused_pos:
+                    avaiPos.append(npos)
+            if len(avaiPos) == 0: break
+            if len(avaiPos) > 1 and dig not in jumpPos: jumpPos.append(dig)
+
+            dpos = choice(avaiPos)
+            unused_pos.remove(dpos)
+            laby.add(dig, dpos)
+            dig = dpos
+        dig = choice(jumpPos)
+
+    return laby
+
+# laby = tunnel()
+# laby.add((0, 0), (1, 0))
+# laby.add((1, 0), (1, 1))
+# laby.add((1, 1), (1, 2))
+
+laby = dig_labyrinth()
+show(laby)
