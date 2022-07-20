fd15c27747a544960fe7ed9004ee02d4a25a74bd
Paul Jaros <jaros.paul@gmail.com>
2022-07-20 14:50:50 +0200

    - Laby-Daten in einer Klasse bereitstellen - Eigene Ebene um Brücken zu speichern

--- laby/arr_1d.py
+++ laby/arr_1d.py
@@ -4,23 +4,33 @@ from time import clock
 import array
 
 
-def dig_laby(sizeX, sizeY):
-    realX, realY = sizeX + 2, sizeY + 2
-    dirs = (realX, 1, -realX, -1)
-    li = array.array("B", [0]) * (realX * realY)
-    for x in range(1, sizeX + 1):
-        for y in range(1, sizeY + 1):
-            li[x + y * realX] = 1
+class Laby:
+    def __init__(self, sizeX, sizeY):
+        self.sizeX = sizeX
+        self.sizeY = sizeY
+        self.realX = sizeX + 2
+        self.realY = sizeY + 2
+        self.realZ = 2
+        self.arr = array.array("B", [0]) * (self.realX * self.realY * self.realZ)
+        for x in range(1, self.sizeX + 1):
+            for y in range(1, self.sizeY + 1):
+                for z in range(2):
+                    self.arr[x + y * self.realX + z * self.realX * self.realY] = 1
+        self.dirs = (self.realX, 1, -self.realX, -1)
+
+
+def create_laby(sizeX, sizeY):
+    li = Laby(sizeX, sizeY)
     jumpPos = []
 
-    pos = 2 * realX + 2
-    li[pos] = 0
+    pos = 2 * li.realX + 2
+    li.arr[pos] = 0
 
     while 1:
         while 1:
             avaiDir = [d
-                       for d in dirs
-                       if li[pos + d * 2] == 1]
+                       for d in li.dirs
+                       if li.arr[pos + d * 2] == 1]
 
             if not avaiDir: break
             dir = choice(avaiDir)
@@ -29,7 +39,7 @@ def dig_laby(sizeX, sizeY):
 
             for i in (0, 1):
                 pos += dir
-                li[pos] = 0
+                li.arr[pos] = 0
 
         if not jumpPos:
             break
@@ -39,27 +49,26 @@ def dig_laby(sizeX, sizeY):
             jumpPos[rand_index] = jumpPos[len(jumpPos)-1]
             jumpPos.pop()
 
-    li[realX + 2] = 0
-    li[realX * sizeY + (realX - 3)] = 0
+    li.arr[li.realX + 2] = 0
+    li.arr[li.realX * sizeY + (li.realX - 3)] = 0
     return li
 
 
-def print_laby(li, sizeX, sizeY):
-    realX, realY = sizeX + 2, sizeY + 2
-    if sizeX <= 77:
-        for i in range(realY):
-            line = ['#' if e == 1 else ' ' for e in li[i * realX:(i + 1) * realX]]
+def print_laby(li):
+    if li.sizeX <= 77:
+        for i in range(li.realY):
+            line = ['#' if e == 1 else ' ' for e in li.arr[i * li.realX:(i + 1) * li.realX]]
             print("".join(line))
 
 
-def paint(laby, sizeX, sizeY, b_size=40, pensize=7, way=None, crosspoint=None, solution=None):
+def paint(li, b_size=40, pensize=7, way=None, crosspoint=None, solution=None):
     global show_solution
     import turtle as t
     # import math
     import sys
 
     screen = t.Screen()
-    width, height = ((sizeX - 1) / 2) * b_size + 40 , ((sizeY - 1) / 2) * b_size + 40
+    width, height = ((li.sizeX - 1) / 2) * b_size + 40 , ((li.sizeY - 1) / 2) * b_size + 40
     screen.setup(width, height, width // 2, height // 2)
     x_off, y_off = -width / 2 + 15, height / 2 - 15
 
@@ -69,37 +78,36 @@ def paint(laby, sizeX, sizeY, b_size=40, pensize=7, way=None, crosspoint=None, s
     def conv_y(y, shift=0.0, b_size=b_size, y_off=y_off):
         return (y / 2) * -b_size + y_off - shift * b_size
 
-    def p_laby_base(laby, sizeX, sizeY, t=t, b_size=b_size):
-        realX, realY = sizeX + 2, sizeY + 2
-        for y in range(1, sizeY+1, 2):
-            for x in range(1, sizeX+1, 2):
-                cur_pos = x + y * realX
-                if li[cur_pos + 1] == 1:
+    def p_laby_base(li, t=t, b_size=b_size):
+        for y in range(1, li.sizeY+1, 2):
+            for x in range(1, li.sizeX+1, 2):
+                cur_pos = x + y * li.realX
+                if li.arr[cur_pos + 1] == 1:
                     t.penup()
                     t.goto(conv_x(x), conv_y(y))
                     t.setheading(0)
                     t.pendown()
                     t.forward(b_size)
-                if li[cur_pos + realX] == 1:
+                if li.arr[cur_pos + li.realX] == 1:
                     t.penup()
                     t.goto(conv_x(x), conv_y(y))
                     t.setheading(270)
                     t.pendown()
                     t.forward(b_size)
 
-    def draw(laby, sizeX, sizeY):
+    def draw(li):
         t.reset()
         t.tracer(0)
         t.hideturtle()
         t.pensize(3)
-        p_laby_base(laby, sizeX, sizeY)
+        p_laby_base(li)
         t.update()
 
     # Focus auf Fenster setzen: https://stackoverflow.com/a/44787756/406423
     rootwindow = screen.getcanvas().winfo_toplevel()
     rootwindow.call('wm', 'attributes', '.', '-topmost', '1')
     rootwindow.call('wm', 'attributes', '.', '-topmost', '0')
-    draw(laby, sizeX, sizeY)
+    draw(li)
     t.listen()
     screen.onkeypress(lambda: sys.exit(0), "Escape")
     screen.mainloop()
@@ -107,19 +115,19 @@ def paint(laby, sizeX, sizeY, b_size=40, pensize=7, way=None, crosspoint=None, s
 
 if __name__ == "__main__":
     # sizeX, sizeY = 3, 3
-    # sizeX, sizeY = 51, 31
+    sizeX, sizeY = 51, 31
     # sizeX, sizeY = 77, 11
     # sizeX, sizeY = 77, 31
-    sizeX, sizeY = 331, 201
+    # sizeX, sizeY = 331, 201
     # sizeX, sizeY = 771, 311
     # sizeX, sizeY = 77711, 711
     start = clock()
-    li = dig_laby(sizeX, sizeY)
+    li = create_laby(sizeX, sizeY)
     end = clock()
-    if sizeX * sizeY <= 77 * 31:
-        print_laby(li, sizeX, sizeY)
+    if li.sizeX * li.sizeY <= 77 * 31:
+        print_laby(li)
     print("Berechnungszeit: %.5f" % (end - start))
 
     if sizeX * sizeY <= 331 * 201:
-        paint(li, sizeX, sizeY, b_size=10)
+        paint(li, b_size=10)
 
