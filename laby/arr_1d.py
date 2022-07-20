606e076ec41ecb090e6721f6ada1180ca8ea5f64
Paul Jaros <jaros.paul@gmail.com>
2022-07-20 08:27:47 +0200

    arr_1d: - Turtle Grafik - Funktioniert als Modul

---
+++ laby/arr_1d.py
@@ -0,0 +1,125 @@
+#!/usr/bin/python
+from random import choice, randint
+from time import clock
+import array
+
+
+def dig_laby(sizeX, sizeY):
+    realX, realY = sizeX + 2, sizeY + 2
+    dirs = (realX, 1, -realX, -1)
+    li = array.array("B", [0]) * (realX * realY)
+    for x in range(1, sizeX + 1):
+        for y in range(1, sizeY + 1):
+            li[x + y * realX] = 1
+    jumpPos = []
+
+    pos = 2 * realX + 2
+    li[pos] = 0
+
+    while 1:
+        while 1:
+            avaiDir = [d
+                       for d in dirs
+                       if li[pos + d * 2] == 1]
+
+            if not avaiDir: break
+            dir = choice(avaiDir)
+            if len(avaiDir) > 1:
+                jumpPos.append(pos)
+
+            for i in (0, 1):
+                pos += dir
+                li[pos] = 0
+
+        if not jumpPos:
+            break
+        else:
+            rand_index = randint(0, len(jumpPos)-1)
+            pos = jumpPos[rand_index]
+            jumpPos[rand_index] = jumpPos[len(jumpPos)-1]
+            jumpPos.pop()
+
+    li[realX + 2] = 0
+    li[realX * sizeY + (realX - 3)] = 0
+    return li
+
+
+def print_laby(li, sizeX, sizeY):
+    realX, realY = sizeX + 2, sizeY + 2
+    if sizeX <= 77:
+        for i in range(realY):
+            line = ['#' if e == 1 else ' ' for e in li[i * realX:(i + 1) * realX]]
+            print("".join(line))
+
+
+def paint(laby, sizeX, sizeY, b_size=40, pensize=7, way=None, crosspoint=None, solution=None):
+    global show_solution
+    import turtle as t
+    # import math
+    import sys
+
+    screen = t.Screen()
+    width, height = ((sizeX - 1) / 2) * b_size + 40 , ((sizeY - 1) / 2) * b_size + 40
+    screen.setup(width, height, width // 2, height // 2)
+    x_off, y_off = -width / 2 + 15, height / 2 - 15
+
+    def conv_x(x, shift=0.0, b_size=b_size, x_off=x_off):
+        return (x / 2) * b_size + x_off + shift * b_size
+
+    def conv_y(y, shift=0.0, b_size=b_size, y_off=y_off):
+        return (y / 2) * -b_size + y_off - shift * b_size
+
+    def p_laby_base(laby, sizeX, sizeY, t=t, b_size=b_size):
+        realX, realY = sizeX + 2, sizeY + 2
+        for y in range(1, sizeY+1, 2):
+            for x in range(1, sizeX+1, 2):
+                cur_pos = x + y * realX
+                if li[cur_pos + 1] == 1:
+                    t.penup()
+                    t.goto(conv_x(x), conv_y(y))
+                    t.setheading(0)
+                    t.pendown()
+                    t.forward(b_size)
+                if li[cur_pos + realX] == 1:
+                    t.penup()
+                    t.goto(conv_x(x), conv_y(y))
+                    t.setheading(270)
+                    t.pendown()
+                    t.forward(b_size)
+
+    def draw(laby, sizeX, sizeY):
+        t.reset()
+        t.tracer(0)
+        t.hideturtle()
+        t.pensize(3)
+        p_laby_base(laby, sizeX, sizeY)
+        t.update()
+
+    # Focus auf Fenster setzen: https://stackoverflow.com/a/44787756/406423
+    rootwindow = screen.getcanvas().winfo_toplevel()
+    rootwindow.call('wm', 'attributes', '.', '-topmost', '1')
+    rootwindow.call('wm', 'attributes', '.', '-topmost', '0')
+    draw(laby, sizeX, sizeY)
+    t.listen()
+    screen.onkeypress(lambda: sys.exit(0), "Escape")
+    screen.mainloop()
+
+
+if __name__ == "__main__":
+    # sizeX, sizeY = 3, 3
+    # sizeX, sizeY = 51, 31
+    # sizeX, sizeY = 77, 11
+    # sizeX, sizeY = 77, 31
+    sizeX, sizeY = 331, 201
+    # sizeX, sizeY = 771, 311
+    # sizeX, sizeY = 77711, 711
+    start = clock()
+    li = dig_laby(sizeX, sizeY)
+    end = clock()
+    if sizeX * sizeY <= 77 * 31:
+        print_laby(li, sizeX, sizeY)
+    print("Berechnungszeit: %.5f" % (end - start))
+
+    if sizeX * sizeY <= 331 * 201:
+        paint(li, sizeX, sizeY, b_size=10)
+
