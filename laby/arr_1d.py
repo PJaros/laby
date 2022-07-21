25e873a1bbef4032e895567d211b17f47fded329
Paul Jaros <jaros.paul@gmail.com>
2022-07-21 10:42:33 +0200

    - Brücken zeichnen - Test V-Brücke

--- laby/arr_1d.py
+++ laby/arr_1d.py
@@ -64,7 +64,7 @@ def print_laby(li):
 def paint(li, b_size=40, pensize=7, way=None, crosspoint=None, solution=None):
     global show_solution
     import turtle as t
-    # import math
+    import math
     import sys
 
     screen = t.Screen()
@@ -110,12 +110,110 @@ def paint(li, b_size=40, pensize=7, way=None, crosspoint=None, solution=None):
                     t.pendown()
                     t.forward(b_size)
 
+    def p_laby_bridge_shadow(li, t=t, b_size=b_size, rel_width=0.4):
+        width = b_size * rel_width
+        rel_lo = (1 - rel_width) / 2
+        rel_hi = 1 - rel_lo
+        # for b in bridge:
+        #     x, y = b.x1, b.y1
+        for y in range(1, li.sizeY, 2):
+            for x in range(1, li.sizeX, 2):
+                cur_pos = (x + 1) + (y + 1) * li.realX  + li.realX * li.realY
+                if li.arr[cur_pos] == 0:
+                    print("0")
+                    t.penup()
+                    if li.arr[cur_pos + li.realX] == 0:
+                        t.setheading(270)
+                        t.goto(conv_x(x, rel_hi), conv_y(y - 2, 0.9))
+                        print("v, x: " + str(x) + ", y: " + str(y))
+                    else:
+                        t.setheading(0)
+                        t.goto(conv_x(x - 2, 0.9), conv_y(y, rel_lo))
+                        print("h, x: " + str(x) + ", y: " + str(y))
+                    bridge_shadow(length=b_size*1.2, width=width)
+
+    def bridge_shadow(length, width, color=(0.8, 0.8, 0.8)):
+        t.penup()
+        t.color(color, color)
+        t.begin_fill()
+        for _ in range(2):
+            t.forward(length)
+            t.right(90)
+            t.forward(width)
+            t.right(90)
+        t.end_fill()
+        t.color("black")
+
+    def p_laby_bridge(bridge, t=t, b_size=b_size, rel_width=0.4, pensize=pensize*0.5):
+        t.pensize(int(pensize*0.70))
+        t.fillcolor("white")
+        width = b_size * rel_width
+        rel_lo = (1 - rel_width) / 2
+        rel_hi = 1 - rel_lo
+        for y in range(1, li.sizeY, 2):
+            for x in range(1, li.sizeX, 2):
+                cur_pos = (x + 1) + (y + 1) * li.realX + li.realX * li.realY
+                if li.arr[cur_pos] == 0:
+                    t.penup()
+                    if li.arr[cur_pos + li.realX] == 0:
+                        t.setheading(270)
+                        t.goto(conv_x(x, rel_hi), conv_y(y - 2, 0.9))
+                    else:
+                        t.setheading(0)
+                        t.goto(conv_x(x - 2, 0.9), conv_y(y, rel_lo))
+                    bridge_arc(length=b_size*1.2, width=width, height=b_size*0.1, pen_size=int(pensize), fill=True)
+                    bridge_arc(length=b_size*1.2, width=width, height=b_size*0.1, pen_size=int(pensize), fill=False)
+
+    def bridge_arc(length, height, width, fill=True, pen_size=3):
+        if fill:
+            t.color("white", "white")
+            t.begin_fill()
+        else:
+            t.color("black", "white")
+        if not fill:
+            t.pensize(pen_size)
+        arc(-height, length)
+        t.right(90)
+        if not fill:
+            t.penup()
+        t.forward(width)
+        t.right(90)
+        if not fill:
+            t.pendown()
+        t.pensize(pen_size)
+        arc(height, length)
+        t.right(90)
+        t.pensize(1)
+        if not fill:
+            t.penup()
+        t.forward(width)
+        t.right(90)
+        if fill:
+            t.end_fill()
+        t.color("black")
+        t.pendown()
+
+    def arc(height, length):
+        abs_height = abs(height)
+        radius = (abs_height ** 2 + (length / 2) ** 2) / (2 * abs_height)
+        winkel = math.atan2(length / 2, radius - abs_height) * 180.0 / math.pi
+        orig_heading = t.heading()
+        if height > 0:
+            t.right(winkel)
+            t.circle(radius, winkel * 2)
+        else:
+            t.left(winkel)
+            t.circle(-radius, winkel * 2)
+        t.setheading(orig_heading)
+
     def draw(li):
         t.reset()
         t.tracer(0)
         t.hideturtle()
         t.pensize(3)
+        p_laby_bridge_shadow(li)
         p_laby_base(li)
+        p_laby_bridge(li)
         t.update()
 
     # Focus auf Fenster setzen: https://stackoverflow.com/a/44787756/406423
@@ -146,7 +244,7 @@ def default_generate_and_paint():
     if sizeX * sizeY <= 331 * 201:
         paint(li, b_size=10)
 
-def test_paint():
+def test_paint_v():
     li = Laby(7, 7)
     li.arr = array.array("B", [
         0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -162,7 +260,31 @@ def test_paint():
         0, 1, 1, 1, 1, 1, 1, 1, 0,
         0, 1, 1, 1, 1, 1, 1, 1, 0,
         0, 1, 1, 1, 0, 1, 1, 1, 0,
+        0, 1, 1, 1, 0, 1, 1, 1, 0,
+        0, 1, 1, 1, 0, 1, 1, 1, 0,
         0, 1, 1, 1, 1, 1, 1, 1, 0,
+        0, 1, 1, 1, 1, 1, 1, 1, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0,
+    ])
+    paint(li)
+
+def test_paint_h():
+    li = Laby(7, 7)
+    li.arr = array.array("B", [
+        0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 1, 0, 1, 1, 1, 1, 1, 0,
+        0, 1, 0, 0, 0, 1, 0, 1, 0,
+        0, 1, 1, 1, 0, 1, 0, 1, 0,
+        0, 1, 0, 1, 0, 1, 0, 1, 0,
+        0, 1, 0, 1, 0, 1, 0, 1, 0,
+        0, 1, 0, 0, 0, 1, 0, 1, 0,
+        0, 1, 1, 1, 1, 1, 0, 1, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 1, 1, 1, 1, 1, 1, 1, 0,
+        0, 1, 1, 1, 1, 1, 1, 1, 0,
+        0, 1, 1, 1, 1, 1, 1, 1, 0,
+        0, 1, 1, 0, 0, 0, 1, 1, 0,
         0, 1, 1, 1, 1, 1, 1, 1, 0,
         0, 1, 1, 1, 1, 1, 1, 1, 0,
         0, 1, 1, 1, 1, 1, 1, 1, 0,
@@ -173,4 +295,5 @@ def test_paint():
 
 if __name__ == "__main__":
     # default_generate_and_paint()
-    test_paint()
+    # test_paint_h()
+    test_paint_v()
